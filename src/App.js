6109d343c7a71e032827a4c28d7eb881c946a123
jumagylov <jumagylov655@gmail.com>
2022-04-19 11:46:26 +0300

    'novoe'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from './Component/Count/Button/Button'
+import{Count} from './Component/Count/Count'
 
 function App() {
   return (
     <div className="App">
-    
+    <Button title='+'/>
+    <Count counter='0'/>
+    <Button title='-'/>
+
     </div>
   );
 }
